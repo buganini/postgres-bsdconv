a87981fdddcaefb863d8a7ce82f7d1608c698c22
Buganini Q <buganini@gmail.com>
2011-11-30 20:47:28 +0800

    now working

--- bsdconv.c
+++ bsdconv.c
@@ -13,22 +13,25 @@ Bsdconv(PG_FUNCTION_ARGS)
 {
 	text *_conversion=PG_GETARG_TEXT_P(0);
 	text *str=PG_GETARG_TEXT_P(1);
-	char *conversion=strndup(VARDATA(_conversion), VARSIZE(_conversion));
+	char *conversion=malloc(VARSIZE(_conversion) - VARHDRSZ + 1);
+	memcpy(conversion, VARDATA(_conversion), VARSIZE(_conversion) - VARHDRSZ);
+	conversion[VARSIZE(_conversion) - VARHDRSZ]=0;
 	struct bsdconv_instance *ins=bsdconv_create(conversion);
 	free(conversion);
 
 	bsdconv_init(ins);
 	ins->output_mode=BSDCONV_PREMALLOCED;
 	ins->input.data=VARDATA(str);
-	ins->input.len=VARSIZE(str);
+	ins->input.len=VARSIZE(str) - VARHDRSZ;
 	ins->input.flags=0;
+	ins->output.data=0;
 	ins->flush=1;
 	bsdconv(ins);
 
 	text *new_text;
 	int32 new_text_size;
 
-	new_text_size=ins->output.len;
+	new_text_size=ins->output.len + VARHDRSZ;
 	new_text=(text *) palloc(new_text_size);
 	ins->output.data=VARDATA(new_text);
 	SET_VARSIZE(new_text, new_text_size);
